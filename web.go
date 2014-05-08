e23edd715c289317a7844c6ac0c756c4951bc322
Joshua Rubin <joshua@rubixconsulting.com>
2014-05-08 13:18:05 -0600

    a few new relic updates
    Signed-off-by: Joshua Rubin <joshua@rubixconsulting.com>

--- web.go
+++ web.go
@@ -2,27 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/yvasiyarov/gorelic"
 )
 
+var agent *gorelic.Agent
+
 func init() {
+	agent = gorelic.NewAgent()
+
 	if key := os.Getenv("NEW_RELIC_LICENSE_KEY"); key != "" {
-		agent := gorelic.NewAgent()
-		agent.Verbose = true
 		agent.NewrelicLicense = key
 		agent.Run()
 	}
 }
 
 func main() {
-	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/", agent.WrapHTTPHandlerFunc(handleIndex))
 
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
 
